d5ed9affe830e3c1eef585d801c803dacf4860cb
syaob <syaob@yahoo.fr>
2024-12-05 15:34:15 +0100

    deploy modif 2

--- public/index.php
+++ public/index.php
@@ -1,3 +1,3 @@
 <?php
     header('Content-Type: text/html; charset=utf-8');
-    echo json_encode(["message" => "salut! hehe modification saikou"]);
+    echo json_encode(["message" => "salut! hehe modification saikou nouvelle modif cc"]);
